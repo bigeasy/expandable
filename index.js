c9102afbd0835dadbe205f50b9051d97eb119c8e
Alan Gutierrez <alan@prettyrobots.com>
2017-04-04 12:33:55 -0500

    Remove semicolons.

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-var path = require('path');
-var fs = require('fs');
+var path = require('path')
+var fs = require('fs')
 
-const REGEX = new RegExp('(\\' + '/ . * + ? | ( ) [ ] { } \\'.split(' ').join('|\\') + ')', 'g');
+var REGEX = new RegExp('(\\' + '/ . * + ? | ( ) [ ] { } \\'.split(' ').join('|\\') + ')', 'g')
 
 function regular (text) { return text.replace(REGEX, '\\$1') }
 
 function find (base, directory, pattern, index, found) {
   if (index == pattern.length) {
-    found.push(path.relative(base, directory));
-    return;
+    found.push(path.relative(base, directory))
+    return
   }
 
   if (pattern[index]('.')) {
-    find(base, directory, pattern, index + 1, found);
-    return;
+    find(base, directory, pattern, index + 1, found)
+    return
   }
 
   if (pattern[index]('..')) {
-    find(base, path.dirname(directory), pattern, index + 1, found);
-    return;
+    find(base, path.dirname(directory), pattern, index + 1, found)
+    return
   }
 
   if (!fs.statSync(directory).isDirectory()) {
@@ -27,82 +27,82 @@ function find (base, directory, pattern, index, found) {
 
   fs.readdirSync(directory).forEach(function (file) {
     if (pattern[index](file)) {
-      find(base, path.resolve(directory, file), pattern, index + 1, found);
+      find(base, path.resolve(directory, file), pattern, index + 1, found)
     }
-  });
+  })
 }
 
 function explode (pattern) {
-  var exploded = [];
+  var exploded = []
   pattern.split('/').forEach(function (unix) {
     unix.split('\\').forEach(function (part) {
-      exploded.push(part);
-    });
-  });
-  return exploded;
+      exploded.push(part)
+    })
+  })
+  return exploded
 }
 
 function alternation ($) {
-  var regex = '(', $1;
+  var regex = '(', $1
   while ($1 = /^([^{}]*)([{}])(.*)$/.exec($[3])) {
-    $ = $1;
-    regex += $[1].split(',').map(function (alt) { return compile(alt) }).join('|');
+    $ = $1
+    regex += $[1].split(',').map(function (alt) { return compile(alt) }).join('|')
     if ($[2] == '}') {
-      regex += ')';
+      regex += ')'
     } else {
-      $ = alternation($);
-      regex += $[1];
+      $ = alternation($)
+      regex += $[1]
     }
   }
-  $[1] = regex;
-  return $;
+  $[1] = regex
+  return $
 }
 
 function compile (glob) {
-  var $, regex = '';
+  var $, regex = ''
   while ($ = /^(.*?)(\*|\?|\[[^\]]+\]|{)(.*)$/.exec(glob)) {
-    regex += regular($[1]);
+    regex += regular($[1])
     switch ($[2][0]) {
     case '*':
       regex += '.*'
-      break;
+      break
     case '?':
       regex += '.'
-      break;
+      break
     case '[':
-      regex += $[2].replace(/^\[!/, '[^');
-      break;
+      regex += $[2].replace(/^\[!/, '[^')
+      break
     case '{':
-      $ = alternation($);
-      regex += $[1];
-      break;
+      $ = alternation($)
+      regex += $[1]
+      break
     }
-    glob = $[3];
+    glob = $[3]
   }
-  return regex + regular(glob);
+  return regex + regular(glob)
 }
 
 function glob (directory, argv) {
-  var found = [];
+  var found = []
   argv.forEach(function (pattern) {
-    var exploded = explode(pattern), match;
+    var exploded = explode(pattern), match
 
     var compiled = exploded.map(function (part) {
-      var regex;
+      var regex
       if (/^\.\.?$/.test(part)) {
         return function (file) { return file == part }
       }
-      regex = new RegExp('^' + compile(part) + '$');
+      regex = new RegExp('^' + compile(part) + '$')
       return function (file) { return !/^\.\.?/.test(file) && regex.test(file) }
-    });
+    })
 
-    found.push(match = { path: pattern, files: [] });
-    exploded = explode(directory);
-    if (exploded[0] == '') exploded[0] = '/';
+    found.push(match = { path: pattern, files: [] })
+    exploded = explode(directory)
+    if (exploded[0] == '') exploded[0] = '/'
     var normalized = path.normalize(path.join.apply(path, exploded))
-    find(normalized, normalized, compiled, 0, match.files);
-  });
-  return found;
+    find(normalized, normalized, compiled, 0, match.files)
+  })
+  return found
 }
 
-exports.glob = glob;
+exports.glob = glob
