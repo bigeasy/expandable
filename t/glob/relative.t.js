ec3a67be8e5c4b94657e018057fd0d3ae01a02c3
Alan Gutierrez <alan@prettyrobots.com>
2017-04-04 12:38:03 -0500

    Indent.

--- t/glob/relative.t.js
+++ t/glob/relative.t.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 require('proof')(2, function (assert) {
-  var glob = require('../..').glob
-  var found
-  found = glob(__dirname + '/../..', [ './t/../t/glob/plain.t.js' ])
-  assert(found[0].files.pop(), 't/glob/plain.t.js', "parent")
-  found = glob(__dirname + '/../..', [ './t/./glob/plain.t.js' ])
-  assert(found[0].files.pop(), 't/glob/plain.t.js', "self")
+    var glob = require('../..').glob
+    var found
+    found = glob(__dirname + '/../..', [ './t/../t/glob/plain.t.js' ])
+    assert(found[0].files.pop(), 't/glob/plain.t.js', "parent")
+    found = glob(__dirname + '/../..', [ './t/./glob/plain.t.js' ])
+    assert(found[0].files.pop(), 't/glob/plain.t.js', "self")
 })
